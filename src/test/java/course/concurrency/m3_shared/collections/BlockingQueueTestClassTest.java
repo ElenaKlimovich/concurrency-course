9e2235d0e034a1127bf62b33a49a4823d52e2066
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-04-16 17:41:26 +0300

    Task 6-1/2 - BlockingQueueTestClassTest tests added

--- src/test/java/course/concurrency/m3_shared/collections/BlockingQueueTestClassTest.java
+++ src/test/java/course/concurrency/m3_shared/collections/BlockingQueueTestClassTest.java
@@ -68,4 +68,106 @@ class BlockingQueueTestClassTest {
         executor.awaitTermination(20_000, TimeUnit.MILLISECONDS);
         assertEquals(0, bq.getCapacity());
     }
+
+    @Test
+    public void testBlocking() throws InterruptedException {
+        BlockingQueueClass<String> bq = new BlockingQueueClass<>();
+
+        for (int it = 0; it < bq.getMaxCapacity(); it++) {
+            bq.enqueue("" + it);
+        }
+
+        assertEquals(bq.getMaxCapacity(), bq.getCapacity());
+
+        Thread producer = new Thread(() -> bq.enqueue("another element"));
+        producer.start();
+        Thread.sleep(100);
+        assertEquals(Thread.State.WAITING, producer.getState());
+
+        Thread consumer = new Thread(() -> {
+            for (int it = 0; it < 100; it++)
+                bq.dequeue();
+
+        });
+        consumer.start();
+        Thread.sleep(100);
+        assertEquals(Thread.State.WAITING, consumer.getState());
+    }
+
+    @Test
+    public void testSimultaneousEnqueueAndDequeue() throws InterruptedException {
+        BlockingQueueClass<String> bq = new BlockingQueueClass<>();
+        ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        for (int i = 0; i < poolSize; i++) {
+            executor.submit(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException ignored) {
+                }
+                for (int it = 0; it < 10_000; it++) {
+                    bq.enqueue("" + it);
+                }
+            });
+
+            executor.submit(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException ignored) {
+                }
+                for (int it = 0; it < 1_000; it++) {
+                    bq.dequeue();
+                }
+            });
+        }
+
+        latch.countDown();
+        executor.shutdown();
+        executor.awaitTermination(10_000, TimeUnit.MILLISECONDS);
+        assertEquals(bq.getMaxCapacity(), bq.getCapacity());
+    }
+
+    @Test
+    public void testOneThread() throws InterruptedException {
+        BlockingQueueClass<String> bq = new BlockingQueueClass<>();
+        ExecutorService single = Executors.newSingleThreadExecutor();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        for (int i = 0; i < poolSize; i++) {
+            single.submit(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException ignored) {
+                }
+                for (int it = 0; it < 1_000; it++) {
+                    bq.enqueue("" + it);
+                    bq.dequeue();
+                }
+            });
+        }
+
+        latch.countDown();
+        single.shutdown();
+        single.awaitTermination(10_000, TimeUnit.MILLISECONDS);
+        assertEquals(0, bq.getCapacity());
+    }
+
+    @Test
+    public void testFifo() {
+        BlockingQueueClass<String> bq = new BlockingQueueClass<>();
+        for (int it = 1; it <= bq.getMaxCapacity(); it++) {
+            bq.enqueue("" + it);
+        }
+
+        String firstElement = bq.dequeue();
+        assertEquals(String.valueOf(1), firstElement);
+
+        while (bq.getCapacity() > 1) {
+            bq.dequeue();
+        }
+
+        String lastElement = bq.dequeue();
+        assertEquals(String.valueOf(bq.getMaxCapacity()), lastElement);
+    }
 }
