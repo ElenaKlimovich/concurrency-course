177e2029489c77e47fabb302064968c2bc3ea7fe
elenaklimovich <eklimovich@systematica-consulting.ru>
2023-04-17 15:09:52 +0300

    6-1/2 - tests refactored

--- src/test/java/course/concurrency/m3_shared/collections/BlockingQueueTestClassTest.java
+++ src/test/java/course/concurrency/m3_shared/collections/BlockingQueueTestClassTest.java
@@ -70,9 +70,8 @@ class BlockingQueueTestClassTest {
     }
 
     @Test
-    public void testBlocking() throws InterruptedException {
+    public void testBlockingWhenAdd() throws InterruptedException {
         BlockingQueueClass<String> bq = new BlockingQueueClass<>();
-
         for (int it = 0; it < bq.getMaxCapacity(); it++) {
             bq.enqueue("" + it);
         }
@@ -84,14 +83,29 @@ class BlockingQueueTestClassTest {
         Thread.sleep(100);
         assertEquals(Thread.State.WAITING, producer.getState());
 
+        bq.dequeue();
+        Thread.sleep(100);
+        assertEquals(Thread.State.TERMINATED, producer.getState());
+    }
+
+    @Test
+    public void testBlockingWhenGet() throws InterruptedException {
+        BlockingQueueClass<String> bq = new BlockingQueueClass<>();
+        bq.enqueue("some value");
         Thread consumer = new Thread(() -> {
-            for (int it = 0; it < 100; it++)
+            while (bq.getCapacity() != 0)
                 bq.dequeue();
 
+            bq.dequeue();
         });
+
         consumer.start();
         Thread.sleep(100);
         assertEquals(Thread.State.WAITING, consumer.getState());
+
+        bq.enqueue("another value");
+        Thread.sleep(100);
+        assertEquals(Thread.State.TERMINATED, consumer.getState());
     }
 
     @Test
@@ -106,7 +120,7 @@ class BlockingQueueTestClassTest {
                     latch.await();
                 } catch (InterruptedException ignored) {
                 }
-                for (int it = 0; it < 10_000; it++) {
+                for (int it = 0; it < 1000; it++) {
                     bq.enqueue("" + it);
                 }
             });
@@ -116,7 +130,7 @@ class BlockingQueueTestClassTest {
                     latch.await();
                 } catch (InterruptedException ignored) {
                 }
-                for (int it = 0; it < 1_000; it++) {
+                for (int it = 0; it < 1000; it++) {
                     bq.dequeue();
                 }
             });
@@ -125,49 +139,38 @@ class BlockingQueueTestClassTest {
         latch.countDown();
         executor.shutdown();
         executor.awaitTermination(10_000, TimeUnit.MILLISECONDS);
-        assertEquals(bq.getMaxCapacity(), bq.getCapacity());
+        assertEquals(0, bq.getCapacity());
     }
 
     @Test
-    public void testOneThread() throws InterruptedException {
+    public void testOneThread() {
         BlockingQueueClass<String> bq = new BlockingQueueClass<>();
-        ExecutorService single = Executors.newSingleThreadExecutor();
-        CountDownLatch latch = new CountDownLatch(1);
 
-        for (int i = 0; i < poolSize; i++) {
-            single.submit(() -> {
-                try {
-                    latch.await();
-                } catch (InterruptedException ignored) {
-                }
-                for (int it = 0; it < 1_000; it++) {
-                    bq.enqueue("" + it);
-                    bq.dequeue();
-                }
-            });
-        }
+        Thread single = new Thread(() -> {
+            for (int it = 0; it < 1_000; it++) {
+                bq.enqueue("" + it);
+                bq.dequeue();
+            }
+        });
 
-        latch.countDown();
-        single.shutdown();
-        single.awaitTermination(10_000, TimeUnit.MILLISECONDS);
+        single.start();
         assertEquals(0, bq.getCapacity());
     }
 
     @Test
     public void testFifo() {
         BlockingQueueClass<String> bq = new BlockingQueueClass<>();
-        for (int it = 1; it <= bq.getMaxCapacity(); it++) {
+        int counter = 1;
+        for (int it = counter; it <= bq.getMaxCapacity(); it++) {
             bq.enqueue("" + it);
         }
 
-        String firstElement = bq.dequeue();
-        assertEquals(String.valueOf(1), firstElement);
+        assertEquals(bq.getMaxCapacity(), bq.getCapacity());
 
-        while (bq.getCapacity() > 1) {
-            bq.dequeue();
+        while (bq.getCapacity() != 0) {
+            String current = bq.dequeue();
+            assertEquals(String.valueOf(counter), current);
+            counter++;
         }
-
-        String lastElement = bq.dequeue();
-        assertEquals(String.valueOf(bq.getMaxCapacity()), lastElement);
     }
 }
